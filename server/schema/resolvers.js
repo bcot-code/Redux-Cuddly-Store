7073e61a0871aadb6b546d97bc211257993e799a
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-03-01 23:26:39 -0600

    fixing the connection

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -20,7 +20,7 @@ const resolvers = {
           path: "orders.products",
           populate: "category",
         });
-        user.orders.sort((a, b) => b.createdAt - a.createdAt);
+        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
         return user;
       }
       throw new AuthenticationError("Not logged in");
@@ -58,7 +58,7 @@ const resolvers = {
       }
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
-        line_items,
+        lineItems,
         mode: "payment",
         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`,
